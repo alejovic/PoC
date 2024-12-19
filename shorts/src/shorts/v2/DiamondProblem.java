167c54d2b26e449d43d432da505feae3521263b0
avictoria <alejovic@gmail.com>
2024-12-19 11:49:59 +0100

    java study

---
+++ shorts/src/shorts/v2/DiamondProblem.java
@@ -0,0 +1,28 @@
+package shorts.v2;
+
+interface I {
+    void doSomething();
+}
+
+class A implements I {
+    @Override
+    public void doSomething() {
+
+    }
+}
+
+class B implements I {
+    @Override
+    public void doSomething() {
+
+    }
+}
+
+// class C extends A,B { -> no compile
+//
+//}
+
+public class DiamondProblem {
+
+
+}
